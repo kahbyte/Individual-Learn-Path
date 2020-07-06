f18bbfd85348f18fd9d695537a812eb07654ad48
Kauê <kaue.sbsousa@gmail.com>
2020-07-06 12:15:18 -0300

    adicionando projeto inicial, finalmente

---
+++ Individual Learn Path/Individual Learn Path/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Individual Learn Path
+//
+//  Created by Kauê Sales on 06/07/20.
+//  Copyright © 2020 Kauê Sales. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
